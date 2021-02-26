a7ffd75fccb12ee33033c990b6bfbffd306f60f0
tineetinytin <christine.elene@gmail.com>
2021-02-26 11:55:20 -0800

    looping a triangle & odd or even

---
+++ script.js
@@ -0,0 +1,24 @@
+//LOOPING A TRIANGLE
+
+// let content = '';
+
+// for (let i = 0; i < 7; i++) {
+//     content = content + '#';
+//     console.log(content);
+// }
+
+//ODD OR EVEN
+
+// for (let x=0; x<=15; x++) {
+//     if (x === 0) {
+//             console.log(x +  " is even");
+//     }
+//     else if (x % 2 === 0) {
+//             console.log(x + " is even");   
+//     }
+//     else {
+//             console.log(x + " is odd");
+//     }
+// }
+
+//MARCO! POLO! 
